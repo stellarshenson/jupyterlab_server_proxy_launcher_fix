210b74c6b3298cba89d0e3dd84525c53a66833ea
stellarshenson <konrad.jelen@gmail.com>
2026-01-20 13:52:56 +0000

    fix: update UI test to match actual activation log message

--- ui-tests/tests/jupyterlab_server_proxy_launcher_fix.spec.ts
+++ ui-tests/tests/jupyterlab_server_proxy_launcher_fix.spec.ts
@@ -16,10 +16,6 @@ test('should emit an activation console message', async ({ page }) => {
   await page.goto();
 
   expect(
-    logs.filter(
-      s =>
-        s ===
-        'JupyterLab extension jupyterlab_server_proxy_launcher_fix is activated!'
-    )
+    logs.filter(s => s === '[server-proxy-launcher-fix] Extension activated')
   ).toHaveLength(1);
 });
